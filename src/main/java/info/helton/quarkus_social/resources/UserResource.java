421133a8552e9aa5e6a6829b75658bdbb7eef013
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-08-03 22:32:18 -0300

    creating the get users endpoint

--- src/main/java/info/helton/quarkus_social/resources/UserResource.java
+++ src/main/java/info/helton/quarkus_social/resources/UserResource.java
@@ -1,18 +1,28 @@
 package info.helton.quarkus_social.resources;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 
 import info.helton.quarkus_social.constants.Route;
 import info.helton.quarkus_social.dto.input.UserIDTO;
 
 @Path(Route.USERS)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
 
     @POST
     public Response create(UserIDTO dto) {
-        return Response.status(Status.CREATED).build();
+        return Response.ok(dto).build();
+    }
+
+    @GET
+    public Response listAll() {
+        return Response.ok().build();
     }
 }
