4b870cb5fb86dbede1b3aedbf31f308df129fcfe
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-08-11 21:49:29 -0300

    creating user entity

---
+++ src/main/java/info/helton/quarkus_social/models/User.java
@@ -0,0 +1,22 @@
+package info.helton.quarkus_social.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+@Entity
+@Table(name = "Users")
+public class User extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String name;
+
+    public Integer age;
+}
