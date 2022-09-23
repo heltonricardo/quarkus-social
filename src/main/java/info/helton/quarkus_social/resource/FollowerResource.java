d5a6b1a98227cb27dcc7ae6c1d0555bb5a32bf48
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-09-22 21:18:06 -0300

    implementing following request

--- src/main/java/info/helton/quarkus_social/resource/FollowerResource.java
+++ src/main/java/info/helton/quarkus_social/resource/FollowerResource.java
@@ -1,9 +1,17 @@
 package info.helton.quarkus_social.resource;
 
 import javax.inject.Inject;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.jboss.resteasy.reactive.RestPath;
 
 import info.helton.quarkus_social.constant.Route;
+import info.helton.quarkus_social.dto.input.FollowerIDTO;
+import info.helton.quarkus_social.model.Follower;
+import info.helton.quarkus_social.model.User;
 import info.helton.quarkus_social.repository.FollowerRepository;
 import info.helton.quarkus_social.repository.UserRepository;
 import info.helton.quarkus_social.template.QSResource;
@@ -13,9 +21,21 @@ import lombok.RequiredArgsConstructor;
 @Path(Route.FOLLOWERS)
 @RequiredArgsConstructor(onConstructor_ = { @Inject })
 public class FollowerResource {
-    
+
     final FollowerRepository repository;
     final UserRepository userRepository;
 
-    
+    @PUT
+    public Response followUser(@RestPath Long userId, FollowerIDTO dto) {
+        User user = userRepository.findById(userId);
+        User follower = userRepository.findById(dto.getFollowerId());
+        if (user == null || follower == null) {
+            return Response.status(Status.NOT_FOUND).build();
+        }
+        Follower entity = new Follower();
+        entity.setUser(user);
+        entity.setFollower(follower);
+        repository.persist(entity);
+        return Response.status(Status.NO_CONTENT).build();
+    }
 }
