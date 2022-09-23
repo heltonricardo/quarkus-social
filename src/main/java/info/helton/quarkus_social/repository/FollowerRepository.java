3af90702c072e37bab67f5b9dbbb52a8ad5efe99
Helton Ricardo <50843386+heltonricardo@users.noreply.github.com>
2022-09-23 19:36:12 -0300

    finalizing follows implementation

--- src/main/java/info/helton/quarkus_social/repository/FollowerRepository.java
+++ src/main/java/info/helton/quarkus_social/repository/FollowerRepository.java
@@ -3,9 +3,17 @@ package info.helton.quarkus_social.repository;
 import javax.enterprise.context.ApplicationScoped;
 
 import info.helton.quarkus_social.model.Follower;
+import info.helton.quarkus_social.model.User;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 
 @ApplicationScoped
 public class FollowerRepository implements PanacheRepository<Follower> {
-    
+
+    public boolean doesNotFollow(User follower, User user) {
+        Parameters params = Parameters.with("follower", follower).and("user", user);
+        PanacheQuery<Follower> query = find("follower =: follower and user =: user", params);
+        return query.firstResultOptional().isEmpty();
+    }
 }
--- src/main/java/info/helton/quarkus_social/resource/FollowerResource.java
+++ src/main/java/info/helton/quarkus_social/resource/FollowerResource.java
@@ -1,6 +1,7 @@
 package info.helton.quarkus_social.resource;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
@@ -26,16 +27,19 @@ public class FollowerResource {
     final UserRepository userRepository;
 
     @PUT
+    @Transactional
     public Response followUser(@RestPath Long userId, FollowerIDTO dto) {
         User user = userRepository.findById(userId);
         User follower = userRepository.findById(dto.getFollowerId());
         if (user == null || follower == null) {
             return Response.status(Status.NOT_FOUND).build();
         }
-        Follower entity = new Follower();
-        entity.setUser(user);
-        entity.setFollower(follower);
-        repository.persist(entity);
+        if (repository.doesNotFollow(follower, user)) {
+            Follower entity = new Follower();
+            entity.setUser(user);
+            entity.setFollower(follower);
+            repository.persist(entity);
+        }
         return Response.status(Status.NO_CONTENT).build();
     }
 }
